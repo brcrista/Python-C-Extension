3d09ff834173327e92803f910c9663de5932809e
Brian Cristante <brcrista@microsoft.com>
2019-08-01 16:50:18 -0400

    spam module is code complete

--- spammodule.c
+++ spammodule.c
@@ -1,31 +1,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-// A custom exception type for this module.
-static PyObject* SpamError;
-
-//! The initialization function for the `spam` module.
-PyMODINIT_FUNC PyInit_spam(void)
-{
-    PyObject* module = PyModule_Create(&spammodule);
-    if (module == NULL)
-    {
-        return NULL;
-    }
-
-    // The Python name for the exception type is `spam.error`
-    // with a base class of `Exception`.
-    SpamError = PyErr_NewException("spam.error", NULL, NULL);
-
-    // Make sure the `spam.error` type object does not get garbage collected
-    // if some Python code deletes it from the module.
-    Py_INCREF(SpamError);
-
-    PyModule_AddObject(module, "error", SpamError);
-    return module;
-}
-
-
 //! Call the C `system` function with the arguments in `args`.
 static PyObject* spam_system(PyObject* self, PyObject* args)
 {
@@ -49,4 +24,47 @@ static PyObject* spam_system(PyObject* self, PyObject* args)
 
     // Return the value as a Python object (an integer).
     return PyLong_FromLong(sts);
+}
+
+// The module's method table
+static PyMethodDef SpamMethods[] =
+{
+    { "system", spam_system, METH_VARARGS, "Execute a command on the underlying shell." },
+    { NULL, NULL, 0, NULL } // Sentinel
+};
+
+static spam_doc = NULL;
+
+// The module's definition
+static struct PyModuleDef spammodule =
+{
+    PyModuleDef_HEAD_INIT,
+    "spam", // name of the module
+    spam_doc, // module documentation
+    -1, // the module keeps state in global variables
+    SpamMethods
+};
+
+// A custom exception type for this module.
+static PyObject* SpamError;
+
+//! The initialization function for the `spam` module.
+PyMODINIT_FUNC PyInit_spam(void)
+{
+    PyObject* module = PyModule_Create(&spammodule);
+    if (module == NULL)
+    {
+        return NULL;
+    }
+
+    // The Python name for the exception type is `spam.error`
+    // with a base class of `Exception`.
+    SpamError = PyErr_NewException("spam.error", NULL, NULL);
+
+    // Make sure the `spam.error` type object does not get garbage collected
+    // if some Python code deletes it from the module.
+    Py_INCREF(SpamError);
+
+    PyModule_AddObject(module, "error", SpamError);
+    return module;
 }
